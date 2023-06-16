cf5f1e369af3c598928ef41d8fc1fa3905f8e439
raisouzabatista <99252065+raisouzabatista@users.noreply.github.com>
2023-06-16 11:57:18 -0300

    Add files via upload

---
+++ Tarefa_um/codigo.js
@@ -0,0 +1,20 @@
+
+  var nome1 = window.prompt("Digite o nome do primeiro caro")
+  var x = window.prompt("Velocidade do carro 1")
+var nome2 = window.prompt("Digite o nome do segundo caro")
+    var y =  window.prompt("Velocidade do carro 2")
+
+if (x > y) {
+        alert("O "+nome1+" é mais veloz que o carro 2")
+    }
+  if( x < y) {
+        alert("O "+nome2+" é mais veloz que o carro 1")
+    }
+if(x == y){
+        alert("a velocidade do "+nome1+" e do "+nome2+" são iguais" )
+    }
+
+
+
+
+
